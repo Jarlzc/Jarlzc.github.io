2257cb1ba73a02e8cf25510cc089e3c6771165b5
Charles Choi <charlesc.inbox@gmail.com>
2020-09-09 09:18:01 +0800

    beta

--- assets/script/javascript.js
+++ assets/script/javascript.js
@@ -18,40 +18,34 @@ $(document).ready(function(){
             navbrand.addClass('scroll-brand');
             navlink.addClass('scroll-link');
 		}
-});
-var gallery = document.querySelector('#gallery');
-var getVal = function (elem, style) { return parseInt(window.getComputedStyle(elem).getPropertyValue(style)); };
-var getHeight = function (item) { return item.querySelector('.content').getBoundingClientRect().height; };
-var resizeAll = function () {
-    var altura = getVal(gallery, 'grid-auto-rows');
-    var gap = getVal(gallery, 'grid-row-gap');
-    gallery.querySelectorAll('.gallery-item').forEach(function (item) {
-        var el = item;
-        el.style.gridRowEnd = "span " + Math.ceil((getHeight(item) + gap) / (altura + gap));
     });
-};
-gallery.querySelectorAll('img').forEach(function (item) {
-    item.classList.add('byebye');
-    if (item.complete) {
-        console.log(item.src);
-    }
-    else {
-        item.addEventListener('load', function () {
-            var altura = getVal(gallery, 'grid-auto-rows');
-            var gap = getVal(gallery, 'grid-row-gap');
-            var gitem = item.parentElement.parentElement;
-            gitem.style.gridRowEnd = "span " + Math.ceil((getHeight(gitem) + gap) / (altura + gap));
-            item.classList.remove('byebye');
-        });
-    }
-});
-window.addEventListener('resize', resizeAll);
-gallery.querySelectorAll('.gallery-item').forEach(function (item) {
-    item.addEventListener('click', function () {        
-        item.classList.toggle('full');        
-    });
-});
 
+    $('.gallery img').click(function () {
+        $(this).animate({
+            width: '200%',
+            jusitifyContent: 'center',
+        
+        }, 500, "linear", function () {
+            console.log('animation complete')
+        }
+        )})
+    $('.gallery img').mouseout(function() {
+        $(this).animate({
+            width: "100%"
+        }, 500, "linear", function() {
+            console.log('animation complete')
+        })
+    })
+
+    $('.gallery img').last().click(function () {
+        $(this).animate({
+            left: "+=200px",
+        }, 500, 'linear', function (){
+            console.log("scrolled")
+        })
+
+    })
+            
 
 
 });
