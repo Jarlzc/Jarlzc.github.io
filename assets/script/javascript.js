7ee6d0666bd033dc70d57e763fd98eb5c949cdfa
Charles Choi <charlesc.inbox@gmail.com>
2020-09-10 08:24:00 +0800

    change using lightbox

--- assets/script/javascript.js
+++ assets/script/javascript.js
@@ -1,8 +1,17 @@
 
 $(document).ready(function(){
+//    hidding creative part
+   
     $("#show").click(function(){
         $(".creative").toggle(1788);
     });
+
+//  lightbox option
+//  lightbox.option({
+//     'resizeDuration': 200,
+//     'wrapAround': true
+//   })
+
 // narbar transform
     var navbar = $('.navbar');
     var navbrand = $('.navbar-brand');
@@ -19,32 +28,32 @@ $(document).ready(function(){
             navlink.addClass('scroll-link');
 		}
     });
-
-    $('.gallery img').click(function () {
-        $(this).animate({
-            width: '200%',
-            jusitifyContent: 'center',
+// gallery
+    // $('.gallery img').click(function () {
+    //     $(this).animate({
+    //         width: '200%',
+    //         zIndex: '2'
         
-        }, 500, "linear", function () {
-            console.log('animation complete')
-        }
-        )})
-    $('.gallery img').mouseout(function() {
-        $(this).animate({
-            width: "100%"
-        }, 500, "linear", function() {
-            console.log('animation complete')
-        })
-    })
+    //     }, 500, "linear", function () {
+    //         console.log('animation complete')
+    //     }
+    //     )})
+    // $('.gallery img').mouseout(function() {
+    //     $(this).animate({
+    //         width: "100%"
+    //     }, 500, "linear", function() {
+    //         console.log('animation complete')
+    //     })
+    // })
 
-    $('.gallery img').last().click(function () {
-        $(this).animate({
-            left: "+=200px",
-        }, 500, 'linear', function (){
-            console.log("scrolled")
-        })
+    // $('.gallery img').last().click(function () {
+    //     $(this).animate({
+    //         left: "+=200px",
+    //     }, 500, 'linear', function (){
+    //         console.log("scrolled")
+    //     })
 
-    })
+    // })
             
 
 
